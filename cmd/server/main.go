6238748b6e87cbf580670b396c1f6fd0949d0073
genus555 <yunxiaoche@gmail.com>
2025-12-16 18:08:53 -0800

    Message Brokers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	connection_string := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connection_string)
+	if err != nil {
+		log.Fatalf("Something went wrong: %v", err)
+	} else {
+		log.Println("Connection was successful")
+	}
+	defer connection.Close()
+
 	fmt.Println("Starting Peril server...")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Connection is closed")
 }
